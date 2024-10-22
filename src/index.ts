72d6e12dc564440401a186b696cc4dca1036e6cb
wilmouths <wilmouthsteven@gmail.com>
2024-10-22 14:44:44 +0200

    refactor: using logger, fastify & puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,50 @@
-import pino, { Logger } from "pino";
-import puppeteer from 'puppeteer';
+import fastify from "fastify";
+import fastifyIO from "fastify-socket.io";
 
-const logger:Logger = pino();
+import Puppeteer from './lib/puppeteer';
+import logger from './lib/logger';
 
+import { name, version } from '../package.json';
 
-(async () => {
+const server = fastify();
+server.register(fastifyIO);
 
-// Launch the browser and open a new blank page
-const browser = await puppeteer.launch({
-  executablePath: '/usr/bin/google-chrome-stable',
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-});
-const page = await browser.newPage();
+const puppeteer = new Puppeteer();
 
-// Navigate the page to a URL.
-await page.goto('https://developer.chrome.com/', {
-  waitUntil: 'networkidle2',
-});
-
-// Set screen size.
-await page.pdf({
-  path: '/pdf/test.pdf',
+server.get('/', (req, reply) => {
+  reply.send({
+    statusCode: 200,
+    message: 'Ping result',
+    data: { name, version },
+  })
 });
 
+server.get('/test', async (req, reply) => {
+  await puppeteer.newPage();
 
-await browser.close();
-})();
+  const pdf = await puppeteer.generatePdfFromHtml('<h1>Hello World !</h1>', null, { format: 'A4' });
+
+  await puppeteer.close();
+
+  reply
+    .type('application/pdf')
+    .header('Content-Disposition', 'inline; filename="generated.pdf"')
+    .send(pdf);
+});
+
+server.ready().then(() => {
+  server.io.on('connection', (socket) => {
+    // TODO: add logic take example on /test
+  });
+});
+
+server.listen({ port: 3000, host: '0.0.0.0' }, async (err, address) => {
+  if (err) {
+    await puppeteer.close();
+    logger.error(err);
+    process.exit(1);
+  }
+  logger.info('Server listening on http://localhost:3000');
+
+  await puppeteer.init();
+});
